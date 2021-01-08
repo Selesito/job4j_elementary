64bcaae737f69f35d3bb4d1814f1f682d1c33b5f
Selesito <selesitomorozov@gmail.com>
2021-01-08 13:37:43 +0400

    2. Scanner и чтение числа из консоли. [#436322]

--- src/ru/job4j/io/Matches.java
+++ src/ru/job4j/io/Matches.java
@@ -8,37 +8,24 @@ public class Matches {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int value = 11;
-        String name = null;
+        int count = 0;
         System.out.println("Игра 11 спичек!");
         System.out.println("Два игрока по очереди берут от 1 до 3 спичек.");
         while (value > 0) {
-            System.out.println("Игорь введите количество спичек");
-            int playerOne = Integer.valueOf(input.nextLine());
-            if (playerOne < 1 || playerOne > 3) {
+            System.out.println("Введите пожалуйста число!");
+            int select = Integer.valueOf(input.nextLine());
+            if (select < 1 || select > 3) {
                 System.out.println("Вы ввели некоректное чило, введите от 1 до 3!");
                 continue;
             }
-            value = value - playerOne;
-            if (value <= 0) {
-                System.out.println("Осталось 0 спичек");
-                name = "Игорь победил!";
-                break;
-            }
-            System.out.println("Осталось " + " " + value + " " + "спичек");
-            System.out.println("Иван введите количество спичек");
-            int playerTwo = Integer.valueOf(input.nextLine());
-            if (playerTwo < 1 || playerTwo > 3) {
-                System.out.println("Вы ввели некоректное чило, введите от 1 до 3!");
-                continue;
-            }
-            value = value - playerTwo;
-            if (value <= 0) {
-                System.out.println("Осталось 0 спичек");
-                name = "Иван победил!";
-                break;
-            }
+            value = value - select;
+            count++;
             System.out.println("Осталось " + " " + value + " " + "спичек");
         }
-        System.out.println(name);
+        if (count % 2 == 0) {
+            System.out.println("Победил Игорь!");
+        } else {
+            System.out.println("Победил Иван!");
+        }
     }
 }
