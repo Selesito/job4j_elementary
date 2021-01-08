1ea6c2c046fb01a34eeeaac823d4278af959cdb2
Selesito <selesitomorozov@gmail.com>
2021-01-08 11:24:39 +0400

    3. Интерфейс Input [#436323]

---
+++ src/ru/job4j/poly/Bus.java
@@ -0,0 +1,17 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+    @Override
+    public void travel() {
+    }
+
+    @Override
+    public void passeger(int count) {
+    }
+
+    @Override
+    public double refuel(double fuel) {
+        double price = fuel * 47.5;
+        return price;
+    }
+}
---
+++ src/ru/job4j/poly/Transport.java
@@ -0,0 +1,9 @@
+package ru.job4j.poly;
+
+public interface Transport {
+    void travel();
+
+    void passeger(int count);
+
+    double refuel(double fuel);
+}
